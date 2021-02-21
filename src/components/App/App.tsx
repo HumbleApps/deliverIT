ec08348388565dc730e4ecd98938ef2fee75540d
Prabhjot <prabhjot.singh@letstransport.team>
2021-02-22 01:01:20 +0530

    Firebase addded

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+
+  return (
+    <Fragment>
+        <Text>This is App page</Text>
+    </Fragment>
+  );
+};
+
+export default App;
